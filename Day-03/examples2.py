2a7a5f5a0f7ef8de8bb5675cc79292966e255bdd
Shay <shaymiyavt@gmail.com>
2026-03-23 20:36:38 +0200

    Finish examples2

---
+++ Day-03/examples2.py
@@ -0,0 +1,19 @@
+print('== Addition, Substraction, Multiplication, Division, Modulus ==')
+
+# Declaring values and organizing them together
+num_one = 3
+num_two = 4
+
+# Arithmetic operations
+total = num_one + num_two
+diff = num_two - num_one
+product = num_one * num_two
+div = num_two / num_one
+remainder = num_two % num_one
+
+# Printing values with label
+print('total: ', total)
+print('difference: ', diff)
+print('product: ', product)
+print('division: ', div)
+print('remainder: ', remainder)
