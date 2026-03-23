93bf54e41e88886779e40ac8d783ac77cadf45ac
Shay <shaymiyavt@gmail.com>
2026-03-23 20:41:22 +0200

    Add third example set

---
+++ Day-03/examples3.py
@@ -0,0 +1,22 @@
+# Calculating area of a circle
+radius = 10
+area_of_circle = 3.14 * radius ** 2
+print('Area of circle: ', area_of_circle)
+
+# Calculating area of a rectangle
+length = 10
+width = 20
+area_of_rectangle = length * width
+print('Area of rectangle: ', area_of_rectangle)
+
+# Calculating a weight of an object
+mass = 75
+gravity = 9.81
+weight = mass * gravity
+print(weight, 'N')
+
+# Calculate the density of a liquid
+mass = 75 # in Kg
+volume = 0.075 # in cubic meter
+density = mass / volume
+print(density, 'Kg/m^3')
