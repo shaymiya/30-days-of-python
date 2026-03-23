c9a0d9be62ae32a2402ee8bc3e80910460b18881
Shay <shaymiyavt@gmail.com>
2026-03-23 20:29:43 +0200

    Write down first examples

---
+++ Day-03/examples1.py
@@ -0,0 +1,21 @@
+# Arithmetic Operations in Python
+# Integer
+
+print('Addition: ', 1 + 2)
+print('Substraction: ', 1 - 2)
+print('Multiplication: ', 2 * 3)
+print('Division: ', 4 / 2)          # division in python gives floating number
+print('Division: ', 6 / 2)
+print('Division: ', 7 / 2)
+print('Division without the remainder: ', 7 // 2)   # tells how many times the whole number fits into the division target
+print('Division without the remainder: ', 7 // 3)
+print('Modulus: ', 3 % 2)           # gives the remainder
+print('Exponentiation: ', 2 ** 3)
+
+# Floating numbers
+print('Floating Point Number, PI', 3.14)
+print('Floating Point Number, gravity', 9.81)
+
+# Complex numbers
+print('Complex number: ', 1 + 1j)
+print('Multiplying complex numbers: ', (1 + 1j) * (1 - 1j))
