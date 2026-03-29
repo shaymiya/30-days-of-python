182f24f860fc7533adffca1f23b83b695929e274
Shay <shaymiyavt@gmail.com>
2026-03-29 22:21:36 +0300

    finish exercises level 2

--- Day-11/day_11.py
+++ Day-11/day_11.py
@@ -96,10 +96,291 @@ def calculate_slope():
                 print("I'm sorry, I can't help you with your equation")
 
 # HOW TO MAKE THIS LOOK NEATER? ^ ;;
-        
+# Welp, at least it works . U . ;;;;
+
 slope = calculate_slope()
 
 if slope:
     print('=====================\nThe slope is:', slope)
+
+print('-----------------------------------')
+
+# Quadratic equation is calculated as follows: ax^2 + bx + c = 0
+# Write a function which calculates solution set of a quadratic equation,
+# solve_quadratic_eqn
+def solve_quadratic_eqn(a, b, c):
+    # print out the steps?
+    print(f'{a}x^2 {'+' if b >= 0 else '-'} {abs(b)}x {'+' if c >= 0 else '-'} {abs(c)} = 0')
+    print('Solving the equation....')
+    print('Using the quadratic equation\'s formula:')
+    print(f'                 _____________________')
+    print(f'     -({b}) +/- v/({b})^2 - 4({a})({c})')
+    print(f' x = ----------------------------------')
+    print(f'                 2({a})')
+    
+    print(f'              ________')
+    print(f'      {-b} +/- v/{b**2} - {4 * a * c}')
+    print(f' x = --------------------')
+    print(f'            {2 * a}')
+
+    print(f'              ____')
+    print(f'      {-b} +/- v/ {b**2 - 4 * a * c}')
+    print(f' x = ---------------')
+    print(f'          {2 * a}')
+
+    print(f'\n       {-b} + {((b**2 - 4 * a * c)**0.5):.2f}           {-b} - {((b**2 - 4 * a * c)**0.5):.2f}')
+    print(f' x = ------------   x = ------------')
+    print(f'           {2 * a}                  {2 * a}')
+
+    print(f'\n Roots: x = {((-b + (b**2 - 4 * a * c)**0.5) / 2 * a):.2f}  and  x = {((-b - (b**2 - 4 * a * c)**0.5) / 2 * a):.2f}')
+
+solve_quadratic_eqn(2, -3, -4)
+
+print('-----------------------------------')
+
+# Declare a function named print_list. It takes list as a parameter and prints out 
+# each element of the list
+def print_list(list):
+    for item in list:
+        print('-', item.title())
+
+fruits = ['banana', 'kiwi', 'avocado', 'apple']
+print_list(fruits)
+
+print('-----------------------------------')
+
+# Declare a function named reverse_list. It takes an array as a parameter and it
+# returns the reverse of the array (use loops)
+def reverse_list(list):
+    temp = []
+    for index in range(len(list) - 1, -1, -1):
+        temp.append(list[index])
+    return temp
+
+print(reverse_list(fruits))
+print(reverse_list([1,2,3,4,5,6,7,8,9,10]))
+
+print('-----------------------------------')
+
+# Declare a function named capitalize_list_items. It takes a list as a parameter and 
+# it returns a capitalized list of items
+def capitalize_list_items(list):
+    temp = []
+    for item in list:
+        temp.append(item.capitalize())
+    return temp
+
+print(capitalize_list_items(fruits))
+
+print('-----------------------------------')
+
+# Declare a function named add_item. It takes a list and an item parameters.
+# It returns a list with the item added at the end.
+def add_item(list, item):
+    list.append(item)
+    return list
+
+print(fruits)
+print(add_item(fruits, 'orange'))
+print(fruits)
+
+print('-----------------------------------')
+
+# Declare a function named remove_item. It takes a list and an item parameters.
+# It returns a list with the item removed from it
+def remove_item(list, item):
+    if item in list: del list[list.index(item)]
+    else: print('Item not found in a list')
+    return list
+
+print(remove_item(fruits, 'apple'))
+print(remove_item(fruits, 'apple'))
+
+print('-----------------------------------')
+
+# Declare a function named sum_of_numbers. It takes number parameter and it adds
+# all the numbers in that range
+def sum_of_numbers(number):
+    sum = 0
+    for num in range(number + 1):
+        sum += num
+    return sum
+
+print(sum_of_numbers(5)) 
+print(sum_of_numbers(10)) 
+print(sum_of_numbers(100)) 
+
+print('-----------------------------------')
+
+# Declare a function named sum_of_odds. It takes a number parameter and it adds all the
+# odd numbers in that range
+def sum_of_odds(number):
+    sum = 0
+    for num in range(number + 1):
+        if num % 2 != 0:
+            sum += num
+    return sum
+
+print('Sum of odds:', sum_of_odds(100))
+
+# Declare a function named sum_of_evens. It takes a number parameter and it adds all the
+# even numbers in that range
+
+def sum_of_evens(number):
+    sum = 0
+    for num in range(number + 1):
+        if num % 2 == 0:
+            sum += num
+    return sum
+
+print('Sum of evens:', sum_of_evens(100))
+
+print('-----------------------------------')
+
+# EXERCISES : LEVEL 2
+
+# Declare a function named evens_and_odds. It takes a positive integer as parameter and it
+# counts number of evens and oddds in the number
+def evens_and_odds(num):
+    evens = 0
+    odds = 0
+    for n in range(num + 1):
+        if n % 2 == 0:
+            evens += 1
+        else:
+            odds += 1
+    return f'The number of odds are: {odds}\nThe number of evens are: {evens}'
+
+print(evens_and_odds(100))
+
+print('-----------------------------------')
+
+# Call  your function factorial. It takes a whole number as a parameter and it returns
+# a factorial of the number
+
+# The factorial of a non-negative integer n is the product of all positive integers 
+# less than or equal to n. (also if n = 0 -> n = 1)
+def factorial(num):
+    product = num if num >= 1 else 1
+    for n in range(num - 1, 0, -1):
+            product *= n
+    return product
+
+print('Factorial of 5 is', factorial(5))
+print('Factorial of 0 is', factorial(0))
+
+print('-----------------------------------')
+
+# Call your function is_empty, it takes a parameter and checks if it is empty or not
+def is_empty(param):
+    if param == None or param == '' or param == 0:
+        return True
+    else:
+        return False
+
+empty_param = None
+
+print('Is parameter empty?', is_empty(fruits))
+print('Is parameter empty?', is_empty(empty_param))
+
+print('-----------------------------------')
+
+# Write different functions which take lists. They should calculate_mean, calculate_median,
+# calculate_mode, calculate_range, calculate_variance and calculate_std (standard deviation)
+numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 1, 10]
+
+# Calculate mean (average)
+def calculate_mean(list): # mean = average of a dataset
+    # let's presume that the lists consist of numbers
+    # average = sum of objects / num of objects
+    sum = 0
+    for num in list:
+        sum += num
+    return sum / len(list)
+print('List\'s mean is:', calculate_mean(numbers))
+
+# Calculate median (the middle number of the list OR average of the two middle nms)
+def calculate_median(list):
+    if len(list) % 2 == 0:
+        return list[int(len(list) / 2)] + list[int(len(list) / 2) + 1] / 2
+    else:
+        return list[int(len(list) / 2)]
+print("List's median is:", calculate_median(numbers))
+
+# Calculate mode: (the value that occurs most often, can have no value, one value or more than one value)
+def calculate_mode(list):
+    # let's create a dictionary and insert list items as keys and the occurrences as values
+    mode_dict = {}
+    for item in list:
+        if mode_dict.get(item) == None:
+            # print('Add new item to the dictionary')
+            mode_dict[item] = 1
+        else:
+            # print('Item already exists in dictionary:', item)
+            mode_dict[item] += 1
+
+    # check if there is no mode in the list:
+    if max(mode_dict.values()) == 1:
+        return 'The list contains no mode'
+    
+    # make a list of top occurrences
+    mode_list = []
+    for k, v in mode_dict.items():
+        if v == max(mode_dict.values()):
+            mode_list.append(k)
+    
+    # check if there are more than 1 modes
+    if len(mode_list) == 1:
+        return f'The list contains following mode: {mode_list[0]}'
+    else:
+        return f'The list contains following modes: {mode_list}'
+
+print(calculate_mode(numbers))
+print(calculate_mode(fruits))
+
+# Calculate range: (highest value - lowest value)
+def calculate_range(list):
+    return max(list) - min(list)
+print("List's range:", calculate_range(numbers))
+
+# Calculate variance: (sum of (n - mean)^2 of each data points / num of data points)
+def calculate_variance(list):
+    sum = 0
+    mean = calculate_mean(list)
+    for num in list:
+        sum += (num - mean) ** 2
+    return sum / len(list)
+print("List's variance:", calculate_variance(numbers))
+
+# Calculate standard deviation: (square root of the variance)
+def calculate_std(list):
+    return calculate_variance(list) ** (1/2)
+print(f"List's standard deviation: {calculate_std(numbers):.2f}")
+
+print('-----------------------------------')
+
+# Write a function called greet which takes default argument, name. If no argument is
+# supplied it should print 'Hello, Guest!', otherwise it should greet the person by name
+def greet(name = 'Guest'):
+    print(f'Hello, {name}!')
+
+greet()
+greet('Shay')
+
+print('-----------------------------------')
+
+# Create a function called show_args to take an arbitary number of named arguments and print
+# their names and values
+def show_args(**args):
+    unpacked_str = 'Received: '
+    for k, v in args.items():
+        unpacked_str += k.strip('\'') + ': ' + str(v).strip('\'') + ', '
+    return unpacked_str[:-2]
+
+print(show_args(name="Alice", age=30, city="New York"))
+
+print('-----------------------------------')
+
+# EXERCISES : LEVEL 3
 
 print('-----------------------------------')
